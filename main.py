674931af0c52f10f461240988726739181d13844
ymophen <96657745+ymophen@users.noreply.github.com>
2022-01-01 22:45:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,51 @@ from colorama import Fore
 import os
 import sys
 import string
-import threading
-from threading import Thread
+g = Fore.GREEN
+r = Fore.RED
+b = Fore.BLUE
+y = Fore.YELLOW
+re = Fore.RESET
+l = Fore.LIGHTBLACK_EX
+blue = Fore.BLUE
+letters = ["a", "b","c", "d","e", "f","g", "h","i", "j","k", "l","m", "n",
+           "o", "p","q", "r","s", "t","u", "v","w", "x","y", "z",]
 
-made = 0
+
+
+
+
+def drawshit():
+    print(f'''
+    
+{g}    
+  /$$$$$$                        /$$     /$$  /$$$$$$          
+ /$$__  $$                      | $$    |__/ /$$__  $$         
+| $$  \__/  /$$$$$$   /$$$$$$  /$$$$$$   /$$| $$  \__//$$   /$$
+|  $$$$$$  /$$__  $$ /$$__  $$|_  $$_/  | $$| $$$$   | $$  | $$
+ \____  $$| $$  \ $$| $$  \ $$  | $$    | $$| $$_/   | $$  | $$
+ /$$  \ $$| $$  | $$| $$  | $$  | $$ /$$| $$| $$     | $$  | $$
+|  $$$$$$/| $$$$$$$/|  $$$$$$/  |  $$$$/| $$| $$     |  $$$$$$$
+ \______/ | $$____/  \______/    \___/  |__/|__/      \____  $$
+          | $$                                        /$$  | $$
+          | $$                                       |  $$$$$$/
+          |__/                                        \______/ 
+ {y}yall Just got raped by a 15 yr old boy LOL 
+ Github.com/ymophen{re}   
+    ''')
+
+
+
+
+
+# Yes i fucked my mind 3 hrs trying to figure out how can i do it and i failed and i raged over my keyboard :D
+def random_char(char_num):
+       return ''.join(random.choice(string.ascii_letters) for _ in range(char_num))
+
+
 url = "https://spclient.wg.spotify.com/signup/public/v1/account"
+
+
 headers = {
   'authority': 'spclient.wg.spotify.com',
   'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
@@ -26,39 +66,32 @@ headers = {
 }
 
 
-def random_char(char_num):
-       return ''.join(random.choice(string.ascii_letters) for _ in range(char_num))
 
-# Yes i fucked my mind 3 hrs trying to figure out how can i do it and i failed and i raged over my keyboard :D
+
+
+
 def mainfunction():
-    global made
-    while True:
-        try:
-            Email = random_char(10)+"@protonmail.com"
-            password = random_char(20)
-            payload = f'birth_day=2&birth_month=02&birth_year=1989&collect_personal_info=undefined&creation_flow=&creation_point=https%3A%2F%2Fwww.spotify.com%2Fus%2F&displayname=DreamyOnTop&gender=male&iagree=1&key=a1e486e2729f46d6bb368d6b2bcda326&platform=www&referrer=&send-email=0&thirdpartyemail=1&email={Email}&password={password}&password_repeat={password}'
+    os.system("cls")
+    drawshit()
+    x = input("Ready? Y/N > ")
+    if x == "y":
+        while True:
+            number = 0
+            Email = random_char(17)+"@protonmail.com"
+            password = random_char(5)+"stephex-777"
+            payload=f'birth_day=2&birth_month=02&birth_year=1989&collect_personal_info=undefined&creation_flow=&creation_point=https%3A%2F%2Fwww.spotify.com%2Fus%2F&displayname=Revengeis1337&gender=male&iagree=1&key=a1e486e2729f46d6bb368d6b2bcda326&platform=www&referrer=&send-email=0&thirdpartyemail=1&email={Email}&password={password}&password_repeat={password}'
             response = requests.request("POST", url, headers=headers, data=payload)
-            print(f"{Email}:{password}")
-            save = open("accounts.txt", "a")
-            save.write(Email + ":" + password + "\n")
-            made+=1
-        except Exception as e:
-            pass
+            print(f"[{g}WORKING{y} PREMIUM{re}] " + Email + ":" + password + " (SAVED)")
+            hits = open("hits.txt","a")
+            hits.write(Email + ":" + password + " | Github.com/ymophen" + "\n")
+            hits.close
 
-def title():
-    global made
-    while True:
-        os.system(f"title Accounts: [{made}]")
-thread = threading.Thread(target=title)
-thread.start()
 
-def start():
-    for i in range(100): 
-        try:
-            threading.Thread(target=mainfunction, args=(), daemon=True).start()
-        except Exception as e:
-            print(e)
-            pass
+
+
+
+
+
 
 if __name__ == "__main__":
-    start()
+    mainfunction()
